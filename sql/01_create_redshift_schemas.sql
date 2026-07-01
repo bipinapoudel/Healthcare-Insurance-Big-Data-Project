6e8166d00f11e1886b72e9caf24084ac888b9319
Bipina Poudel <poudelbipina11@gmail.com>
2026-06-30 23:56:41 -0400

    Added redshift_schemas sql file

---
+++ sql/01_create_redshift_schemas.sql
@@ -0,0 +1,6 @@
+-- 01_create_redshift_schemas.sql
+-- Healthcare Insurance Big Data Project
+-- Purpose: Create Redshift schemas for Silver cleaned data and final project output tables.
+
+CREATE SCHEMA IF NOT EXISTS silver_healthcare;
+CREATE SCHEMA IF NOT EXISTS project_output;
